479b7ad33308087c1bfc143e5e68fc2093a6dc65
josiel-terleski <126162683+josiel-terleski@users.noreply.github.com>
2024-11-29 08:46:01 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,6 +1,8 @@
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json '
+
 async function vizualizarInformacoesGlobais() {
 
-    const res = await fetch(url 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json ')
+    const res = await fetch(url)
     const dados = await res.json()
     //console.log(dados)
     //alt z quebra a linha
